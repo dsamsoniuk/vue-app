6578745225cd63d145b55658e06361cc7e2ff60b
damians <damian@passwordit.pl>
2025-02-26 15:39:27 +0100

    1.3

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
+const { autoUpdater, AppUpdater } = require("electron-updater");
 
 let mainWindow;
+
+autoUpdater.autoDownload = false;
 
 app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
@@ -11,12 +14,42 @@ app.whenReady().then(() => {
     },
   });
 
+  mainWindow.webContents.openDevTools(); // debugger
+
   mainWindow.loadFile("index.html");
 
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+
+  
+  autoUpdater.checkForUpdatesAndNotify();
+  // autoUpdater.checkForUpdates();
+  // mainWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
 });
+
+
+
+autoUpdater.on("update-available", () => {
+  dialog.showMessageBox({
+      type: "info",
+      title: "Aktualizacja dostępna",
+      message: "Nowa wersja jest dostępna. Pobieranie aktualizacji...",
+      buttons: ["OK"]
+  });
+});
+
+autoUpdater.on("update-downloaded", () => {
+  dialog.showMessageBox({
+      type: "info",
+      title: "Aktualizacja gotowa",
+      message: "Aplikacja zostanie teraz zaktualizowana.",
+      buttons: ["Restartuj"]
+  }).then(() => {
+      autoUpdater.quitAndInstall();
+  });
+});
+
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
